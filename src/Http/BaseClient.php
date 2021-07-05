607d4e81c27c416d356bc741e923f760e3e7bbe6
Philip Gichuhi <pgichuhi@microsoft.com>
2021-07-05 15:59:14 +0300

    Renamed Graph to BaseClient

--- src/Http/BaseClient.php
+++ src/Http/BaseClient.php
@@ -3,37 +3,26 @@
 * Copyright (c) Microsoft Corporation.  All Rights Reserved.
 * Licensed under the MIT License.  See License in the project root
 * for license information.
-*
-* Graph File
-* PHP version 7
-*
-* @category  Library
-* @package   Microsoft.Graph
-* @copyright 2016 Microsoft Corporation
-* @license   https://opensource.org/licenses/MIT MIT License
-* @version   GIT: 0.1.0
-* @link      https://graph.microsoft.io/
 */
 
-namespace Microsoft\Graph;
+namespace Microsoft\Graph\Http;
 
 use Microsoft\Graph\Core\GraphConstants;
 use Microsoft\Graph\Core\NationalCloud;
 use Microsoft\Graph\Exception\GraphClientException;
-use Microsoft\Graph\Http\GraphCollectionRequest;
-use Microsoft\Graph\Http\GraphRequest;
-use Microsoft\Graph\Http\HttpClientFactory;
-use Microsoft\Graph\Http\HttpClientInterface;
+use Microsoft\Graph\Exception\GraphException;
 
 /**
- * Class Graph
+ * Class BaseClient
  *
- * @category Library
- * @package  Microsoft.Graph
- * @license  https://opensource.org/licenses/MIT MIT License
- * @link     https://graph.microsoft.io/
+ * Base class to be extended by Graph client classes in v1 and beta packages
+ *
+ * @package Microsoft\Graph
+ * @copyright 2021 Microsoft Corporation
+ * @license https://opensource.org/licenses/MIT MIT License
+ * @link https://developer.microsoft.com/graph
  */
-class Graph
+class BaseClient
 {
     /**
     * The access_token provided after authenticating
@@ -41,7 +30,7 @@ class Graph
     *
     * @var string
     */
-    private $_accessToken;
+    private $accessToken;
 
     /**
     * The api version to use ("v1.0", "beta")
@@ -49,38 +38,38 @@ class Graph
     *
     * @var string
     */
-    private $_apiVersion = GraphConstants::API_VERSION;
+    private $apiVersion = GraphConstants::API_VERSION;
 
     /**
      * Host to use as the base URL and for authentication
      * @var string
      */
-    private $_nationalCloud = NationalCloud::GLOBAL;
+    private $nationalCloud = NationalCloud::GLOBAL;
 
     /**
      * HttpClient to use for requests
      * @var HttpClientInterface
      */
-    private $_httpClient;
+    private $httpClient = null;
 
 
     /**
-     * Graph constructor.
+     * BaseClient constructor.
      *
      * Creates a Graph client object used to make requests to the Graph API
      *
      * @param string|null $apiVersion if null|"" defaults to "v1.0"
      * @param string|null $nationalCloud if null defaults to "https://graph.microsoft.com"
-     * @param HttpClientInterface|null $httpClient null creates default Guzzle client
+     * @param HttpClientInterface|null $httpClient if null creates default Guzzle client
      * @throws GraphClientException
      */
     public function __construct(?string $apiVersion = GraphConstants::API_VERSION,
                                 ?string $nationalCloud = NationalCloud::GLOBAL,
                                 ?HttpClientInterface  $httpClient = null)
     {
-        $this->_apiVersion = ($apiVersion) ?: GraphConstants::API_VERSION;
-        $this->_nationalCloud = ($nationalCloud) ?: NationalCloud::GLOBAL;
-        $this->_httpClient = ($httpClient) ?: HttpClientFactory::nationalCloud($nationalCloud)::createAdapter();
+        $this->apiVersion = ($apiVersion) ?: GraphConstants::API_VERSION;
+        $this->nationalCloud = ($nationalCloud) ?: NationalCloud::GLOBAL;
+        $this->httpClient = ($httpClient) ?: HttpClientFactory::nationalCloud($nationalCloud)::createAdapter();
     }
 
     /**
@@ -90,12 +79,42 @@ class Graph
     * @param string $accessToken The user's access token, retrieved from
     *                     MS auth
     *
-    * @return Graph object
+    * @return $this object
     */
-    public function setAccessToken($accessToken)
+    public function setAccessToken(string $accessToken): self
     {
-        $this->_accessToken = $accessToken;
+        $this->accessToken = $accessToken;
         return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAccessToken(): string {
+        return $this->accessToken;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiVersion(): string {
+        return $this->apiVersion;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNationalCloud(): string
+    {
+        return $this->nationalCloud;
+    }
+
+    /**
+     * @return HttpClientInterface
+     */
+    public function getHttpClient(): HttpClientInterface
+    {
+        return $this->httpClient;
     }
 
 	/**
@@ -106,17 +125,17 @@ class Graph
 	 *
 	 * @return GraphRequest The request object, which can be used to
 	 *                      make queries against Graph
-	 * @throws Exception\GraphException
+	 * @throws GraphException
 	 */
-    public function createRequest($requestType, $endpoint)
+    public function createRequest(string $requestType, string $endpoint): GraphRequest
     {
         return new GraphRequest(
             $requestType,
             $endpoint,
-            $this->_accessToken,
-            $this->_nationalCloud,
-            $this->_apiVersion,
-            $this->_httpClient
+            $this->accessToken,
+            $this->nationalCloud,
+            $this->apiVersion,
+            $this->httpClient
         );
     }
 
@@ -129,17 +148,17 @@ class Graph
 	 *
 	 * @return GraphCollectionRequest The request object, which can be
 	 *                                used to make queries against Graph
-	 * @throws Exception\GraphException
+	 * @throws GraphException
 	 */
-    public function createCollectionRequest($requestType, $endpoint)
+    public function createCollectionRequest(string $requestType, string $endpoint): GraphRequest
     {
         return new GraphCollectionRequest(
             $requestType,
             $endpoint,
-            $this->_accessToken,
-            $this->_nationalCloud,
-            $this->_apiVersion,
-            $this->_httpClient
+            $this->accessToken,
+            $this->nationalCloud,
+            $this->apiVersion,
+            $this->httpClient
         );
     }
 }
