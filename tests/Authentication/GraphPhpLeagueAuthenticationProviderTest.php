37e4d4642bf850065e769109ead6b6e1c9493d30
Philip Gichuhi <pgichuhi@microsoft.com>
2024-12-10 12:35:49 +0300

    fix: Mark nationalCloud constructor parameter in GraphPhpLeagueAuthenticationProvider as deprecated

--- tests/Authentication/GraphPhpLeagueAuthenticationProviderTest.php
+++ tests/Authentication/GraphPhpLeagueAuthenticationProviderTest.php
@@ -4,6 +4,7 @@ namespace Microsoft\Graph\Core\Test\Authentication;
 
 use Microsoft\Graph\Core\Authentication\GraphPhpLeagueAccessTokenProvider;
 use Microsoft\Graph\Core\Authentication\GraphPhpLeagueAuthenticationProvider;
+use Microsoft\Graph\Core\NationalCloud;
 use Microsoft\Kiota\Authentication\Oauth\ClientCredentialContext;
 use PHPUnit\Framework\TestCase;
 
@@ -14,5 +15,13 @@ class GraphPhpLeagueAuthenticationProviderTest extends TestCase
         $context = new ClientCredentialContext('tenant', 'clientId', 'secret');
         $leagueAuthProvider = new GraphPhpLeagueAuthenticationProvider($context, []);
         $this->assertNotEmpty($leagueAuthProvider->getAccessTokenProvider()->getAllowedHostsValidator()->getAllowedHosts());
+    }
+
+    public function testNationalCloudUrlIsUsed(): void
+    {
+        $context = new ClientCredentialContext('tenant', 'clientId', 'secret');
+        $leagueAuthProvider = new GraphPhpLeagueAuthenticationProvider($context, [], NationalCloud::US_GOV);
+        $this->assertEquals('https://login.microsoftonline.us/tenant/oauth2/v2.0/authorize', $leagueAuthProvider->getAccessTokenProvider()->getOauthProvider()->getBaseAuthorizationUrl());
+        $this->assertEquals('https://login.microsoftonline.us/tenant/oauth2/v2.0/token', $leagueAuthProvider->getAccessTokenProvider()->getOauthProvider()->getBaseAccessTokenUrl([]));
     }
 }
